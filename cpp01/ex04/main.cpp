76b6ad22da0dffde342bf96b132c8c29c8be9d25
Romain MOUDURIER <rmouduri@e2r7p2.clusters.42paris.fr>
2022-01-26 12:06:42 +0100

    [ADD] ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		std::cout << "Need 3 parameters" << std::endl;
+		return 1;
+	}
+
+	std::ifstream ifs(av[1], std::ifstream::in);
+	if (ifs)
+	{
+		int i = 0;
+		std::stringstream buf;
+		std::string s;
+		std::string s1(av[1]);
+		std::string s2(av[2]);
+		std::string s3(av[3]);
+		std::ofstream ofs((s1 + ".replace").c_str(), std::ofstream::out);
+
+		if (!ofs)
+		{
+			ifs.close();
+			std::cout << "Can't open " << s1 << ".replace" << std::endl;
+			return 3;
+		}
+		buf << ifs.rdbuf();
+		ifs.close();
+		s = buf.str();
+		while (s[i])
+		{
+			if (s2.length() && s.compare(i, s2.length(), s2) == 0)
+			{
+				s.erase(i, s2.length());
+				s.insert(i, s3);
+			}
+			++i;
+		}
+		ofs << s;
+		ofs.close();
+	}
+	else
+	{
+		std::cout << "Invalid File" << std::endl;
+		return 2;
+	}
+	return 0;
+}
