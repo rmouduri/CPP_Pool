548b895fafaf8762e5f429f8ede581d66bba5df6
Romain MOUDURIER <rmouduri@e2r13p4.clusters.42paris.fr>
2022-02-18 10:38:38 +0100

    [FIX] added test for invalid Bureaucrat instancing

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -9,25 +9,31 @@ int main(void)
 
 	std::cout << std::endl << noName << std::endl << burA << std::endl << burB << std::endl << std::endl;
 
-	try
-	{
+	try {
 		noName.promote(); // 150 -> 149
 		noName.demote(); // 149 -> 150
 		noName.demote(); // 150 -> 151 = throw
-	}
-	catch (std::exception & e)
-	{
+	} catch (std::exception & e) {
 		std::cout << e.what() << std::endl << std::endl;
 	}
 
-	try
-	{
+	try {
 		burA.demote(); // 1 -> 2
 		burA.promote(); // 2 -> 1
 		burA.promote(); // 1 -> 0 = throw
+	} catch (std::exception & e) {
+		std::cout << e.what() << std::endl << std::endl;
 	}
-	catch (std::exception & e)
-	{
+
+	try {
+		Bureaucrat burC("Too high", 0);
+	} catch (std::exception & e) {
+		std::cout << e.what() << std::endl << std::endl;
+	}
+
+	try {
+		Bureaucrat burD("Too low", 151);
+	} catch (std::exception & e) {
 		std::cout << e.what() << std::endl << std::endl;
 	}
 	return 0;
